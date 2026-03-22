f4aedd65d0fa5e7207ce0c78e4e5893b997de265
chinmaypuhan <chinmaypuhan420@gmail.com>
2026-03-22 11:30:00 +0530

    edit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://promodpradhan369_db_user:iBbShegjrReFnIS0@cluster0.ijwxkl1.mongodb.net/note");
         console.log("✅ MongoDB Connected");
     } catch (error) {
         console.error("❌ DB Error:", error.message);
